b4e01522123854960c721817a6d9ec4a0cdf2153
Ironrnd <ironrnd@gmail.com>
2017-04-30 17:29:16 +0300

    Initial Commit

---
+++ Sender/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sender
+//
+//  Created by Andrew Belozerov on 30.04.17.
+//  Copyright © 2017 Andrew Belozerov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
